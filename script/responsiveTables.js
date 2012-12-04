a521e928ece0d202c97f9eb72945d1462468eeac
Christopher D'Alessio <cdalessio@eldamar.net>
2012-12-03 20:11:23 -0600

    A bit of cleanup

--- script/responsiveTables.js
+++ script/responsiveTables.js
@@ -18,6 +18,33 @@
 	var rows = table.querySelectorAll("tr"), rowArray = [], columns = [], headColumn;
 	var firstDataColumn = 1;
 	
+	// utility functions that most libraries will provide in one way or another
+	getNumFromProp = function(p) {
+		return Number(p.replace(/[^0-9]/g,""));	
+	}
+	var getBorderWidths = function(el) {
+		var st = w.getComputedStyle(el);
+		return {
+			"top":st.getPropertyValue("border-top-width"), 
+			"right":st.getPropertyValue("border-right-width"), 
+			"bottom":st.getPropertyValue("border-bottom-width"), 
+			"left":st.getPropertyValue("border-left-width")
+		};
+	}
+	var getWidth = function(el) {
+		var wd = el.clientWidth;
+		var wds = getBorderWidths(el);
+		wd = wd + getNumFromProp(wds.right) + getNumFromProp(wds.left);
+		return wd;
+	}
+	var setStyle = function(el,styles) {
+		var styleString = "", key;
+		for (key in styles) {
+			styleString += key + ":" + styles[key] + ";";
+		}
+		el.setAttribute("style", styleString);
+	}
+	
 	// let the table define itself with proper cell widths so we can get dimensions properly
 	wrapper.setAttribute("style","visibility: hidden; width: 100000px;");
 	var numberOfRows = rows.length, numberOfColumns, rowHeights = [];
@@ -34,7 +61,7 @@
 			if (j === 0 && currentCell.nodeName.toLowerCase() != "th") {
 				firstDataColumn = 0;
 			}
-			currentCell.setAttribute("style","height:"+h+"px");
+			setStyle(currentCell, {"height":h+"px"});
 		}
 	}
 	
@@ -42,7 +69,7 @@
 	wrapper.removeAttribute("style");
 	var currentClassNames = table.getAttribute("class") || "";
 	table.setAttribute("class", currentClassNames + " responsive");
-	var columnWidth = (columns[0][0].clientWidth + Number(w.getComputedStyle(columns[1][1],null).getPropertyValue("border-right-width").replace(/[^0-9]/g,"")));
+	var columnWidth = getWidth(columns[1][1]);
 	var currentDisplayedColumn = 1;
 	
 	var changeDisplayedColumn = function(colIndex) {
@@ -54,7 +81,8 @@
 		currentDisplayedColumn = colIndex + 1;
 		for (var i = 0; i < numberOfRows; i++) {
 			// we'll always set margin on the 1st column of data - this leaves row headings in place
-			columns[firstDataColumn][i].setAttribute("style","height: " + rowHeights[i] + "px; margin-left: -" + (colIndex * columnWidth) + "px;");
+			//columns[firstDataColumn][i].setAttribute("style","height: " + rowHeights[i] + "px; margin-left: -" + (colIndex * columnWidth) + "px;");
+			setStyle(columns[firstDataColumn][i],{"height":rowHeights[i]+"px","margin-left":(-1*(colIndex * columnWidth))+"px"});
 		}	
 	};
 	
@@ -63,23 +91,25 @@
 	elementStore["div"] = d.createElement("div"), elementStore["span"] = d.createElement("span");
 	
 	var dropdown = elementStore["div"].cloneNode();
-	dropdown.setAttribute("class","colHeadingDropdown");
-	var borderWidth = Number(w.getComputedStyle(dropdown,null).getPropertyValue("border-left-width").replace(/[^0-9]/g,"")) + Number(w.getComputedStyle(dropdown,null).getPropertyValue("border-right-width").replace(/[^0-9]/g,""));
-	var dropdownItems = [], dropdownWidth = (columnWidth - borderWidth) + "px";
-	dropdown.setAttribute("style","width: " + dropdownWidth);
+	dropdown.setAttribute("class", "colHeadingDropdown");
+	var dropdownBorderWidths = getBorderWidths(dropdown);
+	var hBorderSum = getNumFromProp(dropdownBorderWidths.left) + getNumFromProp(dropdownBorderWidths.right);
+	var dropdownItems = [], dropdownWidth = (columnWidth - hBorderSum) + "px";
+	setStyle(dropdown, {"width": dropdownWidth});
 	// create options
 	for (var i = 0; i < numberOfColumns; i++) {
 		var option = dropdownItems[i] = elementStore["div"].cloneNode();
 		option.innerHTML = rowArray[0][i].innerHTML + "&nbsp;";
-		option.setAttribute("data-index",i);
+		option.setAttribute("data-index", i);
 		dropdown.appendChild(option);
 	}
 	// make sure the dimensions are what we want them to be
-	dropdown.setAttribute("style","visibility: hidden; width: " + dropdownWidth);
+	setStyle(dropdown,{"visibility":"hidden","width":dropdownWidth});
 	wrapper.appendChild(dropdown);
-	var dropdownHeight = Number(w.getComputedStyle(dropdown.children[1],null).getPropertyValue("border-top-width").replace(/[^0-9]/g,""))
-	var dropdownHeight = numberOfRows * (dropdownItems[0].clientHeight + dropdownHeight);
-	dropdown.setAttribute("style","opacity: 0; height: 0; width: " + dropdownWidth);
+	var dItemBorders = getBorderWidths(dropdown.children[1]);
+	var dropdownHeight = getNumFromProp(dItemBorders.top) + getNumFromProp(dItemBorders.bottom);
+	dropdownHeight = numberOfRows * (dropdownItems[0].clientHeight + dropdownHeight);
+	setStyle(dropdown,{"opacity": 0,"height": 0, "width": dropdownWidth});
 	
 	var showDropdown = function() {
 		for (var i = 0; i < numberOfColumns; i++) {
@@ -89,19 +119,20 @@
 				dropdownItems[i].removeAttribute("class");
 			}
 		}
-		dropdown.setAttribute("style","height: " + dropdownHeight + "px; width: " + dropdownWidth)
+		setStyle(dropdown, {"height": dropdownHeight+"px", "width": dropdownWidth});
 	};
 	var selectColumn = function(e) {
 		var index = Number(e.target.getAttribute("data-index")) || Number(e.target.parentNode.getAttribute("data-index"));
 		if (index >= firstDataColumn) {
 			changeDisplayedColumn((index - 1));
 		}
-		dropdown.setAttribute("style","opacity: 0; height: 0; width: " + dropdownWidth);
+		setStyle(dropdown, {"opacity": 0, "height": 0, "width": dropdownWidth});
 	};
 	
 	var icon = elementStore["span"].cloneNode();
 	icon.setAttribute("class","actionIcon");
 	wrapper.appendChild(icon);
+	icon.addEventListener("click", showDropdown);
 	var thead = table.querySelector("thead");
 	thead.addEventListener("click", showDropdown);
 	dropdown.addEventListener("click", selectColumn);
